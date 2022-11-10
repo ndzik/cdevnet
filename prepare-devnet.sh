b9c8a10a4fdc4a2f1a7ef895c06b3de5bd58b9d0
Jan Bormet <jan.bormet@bormet.info>
2022-11-10 17:27:52 +0100

    Config: Add protocol-parameter and Adjust network start times to correctly reflect POSIXTime

--- prepare-devnet.sh
+++ prepare-devnet.sh
@@ -9,6 +9,7 @@ TARGETDIR="devnet"
 STARTTIME=$(date +%s)
 SYSTEMSTART=$(date -u +%FT%TZ)
 NODE_SOCKET_PATH="$(pwd)/$TARGETDIR/ipc/node.socket"
+PROTOCOL_PARAMS_PATH="$(pwd)/$TARGETDIR/pparams.json"
 
 [ -d "$TARGETDIR" ] && { echo "Cleaning up directory $TARGETDIR" ; sudo rm -r $TARGETDIR ; }
 
@@ -17,9 +18,10 @@ cp -af "$BASEDIR/config/credentials" "$TARGETDIR"
 mkdir $TARGETDIR/wallet-db
 echo '{"Producers": []}' > "$TARGETDIR/topology.json"
 sed -i "s/\"startTime\": [0-9]*/\"startTime\": $STARTTIME/" "$TARGETDIR/genesis-byron.json" && \
-sed -i "s/\"scSlotZeroTime\": [0-9]*/\"scSlotZeroTime\": ${STARTTIME}0000/" "$TARGETDIR/pci-config.json" && \
-sed -i "s/scSlotZeroTime: [0-9]*/scSlotZeroTime: ${STARTTIME}0000/" "$TARGETDIR/perun-pab.yaml" && \
+sed -i "s/\"scSlotZeroTime\": [0-9]*/\"scSlotZeroTime\": ${STARTTIME}000/" "$TARGETDIR/pci-config.json" && \
+sed -i "s/scSlotZeroTime: [0-9]*/scSlotZeroTime: ${STARTTIME}000/" "$TARGETDIR/perun-pab.yaml" && \
 sed -i "s+pscSocketPath: .*+pscSocketPath: ${NODE_SOCKET_PATH}+" "$TARGETDIR/perun-pab.yaml" && \
+sed -i "s+pscProtocolParametersJsonPath: .*+pscProtocolParametersJsonPath: ${PROTOCOL_PARAMS_PATH}+" "$TARGETDIR/perun-pab.yaml" && \
 sed -i "s/\"systemStart\": \".*\"/\"systemStart\": \"$SYSTEMSTART\"/" "$TARGETDIR/genesis-shelley.json"
 
 find $TARGETDIR -type f -exec chmod 0400 {} \;
